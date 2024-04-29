8ad8e25d2f6bf1f803770646b0b9a5da5721fd66
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2024-04-29 18:56:19 +0530

    code updated

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ title, description }) => {
   return (
-    <div className="shadow-xl p-10 transition duration-300 hover:scale-105">
+    <div className="shadow-xl p-10 transition duration-300 hover:scale-105 h-fit md:h-52 w-full">
       <h1 className="text-xl text-blue-500 font-bold">{title}</h1>
       <p>{description}</p>
     </div>
--- src/components/VisionMission/VisionMission.tsx
+++ src/components/VisionMission/VisionMission.tsx
@@ -4,7 +4,7 @@ const VisionMission = () => {
   return (
     <div className="text-center">
       <p className="text-xs md:text-lg font-bold italic">VISION & MISSION</p>
-      <div className="flex justify-center my-2">
+      <div className="flex justify-center items-center my-2">
         <button className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-4 hover:bg-orange-700">
           AFFORDABILITY
         </button>
