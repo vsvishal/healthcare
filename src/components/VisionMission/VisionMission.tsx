1cbcb8a0147e98e8d7a45540d74e82c184ff83ad
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2024-04-29 21:18:33 +0530

    title updated

--- src/components/VisionMission/VisionMission.tsx
+++ src/components/VisionMission/VisionMission.tsx
@@ -5,19 +5,19 @@ const VisionMission = () => {
       <div className="flex justify-center items-center my-2">
         <button
           type="button"
-          className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-4 hover:bg-orange-700"
+          className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-2 md:ml-4 hover:bg-orange-700"
         >
           AFFORDABILITY
         </button>
         <button
           type="button"
-          className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-4 hover:bg-orange-700"
+          className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-2 md:ml-4 hover:bg-orange-700"
         >
           AVAILABILITY
         </button>
         <button
           type="button"
-          className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-4 hover:bg-orange-700"
+          className="bg-orange-500 text-white text-xs md:text-sm font-bold px-2.5 py-1 rounded-sm ml-2 md:ml-4 hover:bg-orange-700"
         >
           ACCESSIBILITY
         </button>
